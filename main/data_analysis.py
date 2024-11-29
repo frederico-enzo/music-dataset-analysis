2a0bc7382d4c3b46a24142ff7f1187e7879d73b3
Frederico <frederico.enzo@hotmail.com>
2024-11-28 21:01:19 -0300

    feat: carregar o arquivo enviado para inspecionar os dados

---
+++ main/data_analysis.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# Caminho do arquivo enviado
+file_path = '/mnt/data/world_data.csv'
+
+# Carregar os dados
+data = pd.read_csv(file_path)
+import matplotlib.pyplot as plt
+
+
