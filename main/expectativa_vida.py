43519d3362eb684738a645f2207ef171de091aaa
Frederico <frederico.enzo@hotmail.com>
2024-12-04 14:52:48 -0300

    style: aprimoramento do gráfico de linhas: ajuste de escala, ticks, e posição da legenda

--- main/expectativa_vida.py
+++ main/expectativa_vida.py
@@ -28,19 +28,19 @@ plt.xlabel("Ano", fontsize=14)
 plt.ylabel("População Acima de 65 Anos (%)", fontsize=14)
 
 # Ajustar o eixo y para uma escala mais adequada
-plt.ylim(0, 40)  # Ajustar limite superior, dependendo dos dados
+plt.ylim(0, 28)  # Ajustar limite superior, dependendo dos dados
 
 # Ajustar os ticks do eixo Y para 4 em 4
-plt.yticks(range(0, 29, 4))  # De 0 até 40, com incremento de 4
+plt.yticks(range(0, 25, 4))  # De 0 até 40, com incremento de 4
 
 # Ajustar a exibição do eixo X para todos os anos
 plt.xticks(ticks=sorted(line_chart_data['ano'].unique()), rotation=45, fontsize=12)
 
 # Adicionar legenda e ajustar posição
-plt.legend(title="País", fontsize=12, loc='upper left')
+plt.legend(title="Países", fontsize=12, title_fontsize=14, loc='upper left', bbox_to_anchor=(1, 1), borderpad=1)
 
 # Melhorar a grade do gráfico com mais visibilidade
-plt.grid(visible=True, linestyle='--', alpha=0.8, linewidth=1.5)  # Ajuste de alpha e linewidth
+plt.grid(visible=True, linestyle='--', alpha=0.7, linewidth=1.5)  # Ajuste de alpha e linewidth
 
 # Ajustar layout para melhor visibilidade
 plt.tight_layout()
